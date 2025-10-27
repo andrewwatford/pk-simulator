1a6754dc461a1689c2f4e1f718f9338c6700f77c
andrewwatford <andrew.watford@dtc.ox.ac.uk>
2025-10-27 16:01:27 +0000

    Change to linear models

--- new_prototype.py
+++ new_prototype.py
@@ -4,8 +4,11 @@ volumes_dict = {'central': 22, 'peripheral': 7}
 model = CompartmentModel(compartment_names = list(volumes_dict.keys()), compartment_volumes = list(volumes_dict.values()))
 model.add_flux(from_compartment = 'central', 
                to_compartment = 'peripheral', 
-               rate_function = first_order_flux)
+               rate_law='first', # or 'zero'
+                rate_constant=5)
 model.add_dosage(compartment = 'central', dosage_function = constant_dose)
-model.add_clearance(compartment = 'central', clearance_function = first_order_clearance)
+model.add_clearance(compartment = 'central', 
+                    rate_law='first', # or 'zero'
+                    rate_constant=5)
 out = model.run(initial_conditions = [0, 0], final_time = 10)
 
--- pkmodel/CompartmentModel.py
+++ pkmodel/CompartmentModel.py
@@ -1,23 +1,43 @@
 from collections import OrderedDict
 from Compartment import Compartment
 from Flux import Flux
+from scipy.integrate import solve_ivp
+import numpy as np
 
 class CompartmentModel:
 
     def __init__(self, compartment_names, compartment_volumes):
-        self.compartments = OrderedDict()
-        self.fluxes = []
-        for name, vol in zip(compartment_names, compartment_volumes):
-            self.compartments[name] = Compartment(name, vol)
+        self.compartment_names = compartment_names
+        self.compartment_volumes = compartment_volumes
+        self.num_compartments = len(self.compartment_names)
+        self.rhs_matrix = np.zeros((self.num_compartments, self.num_compartments))
+        self.rhs_cst_vector = np.zeros((self.num_compartments))
 
-    def add_dosage(self, compartment_name, dosage_function):
-        self.compartments[compartment_name].set_dosage(dosage_function)
-
-    def add_clearance(self, compartment_name, clearance_function):
-        self.compartments[compartment_name].set_clearance(clearance_function)
-
-    def add_flux(self, from_compartment, to_compartment, rate_function):
-        self.fluxes.append(Flux(from_compartment, to_compartment, rate_function))
-            
+    def add_flux(self, from_compartment, to_compartment, rate_constant, rate_law='first'):
+        source_idx = self.compartment_names.index(from_compartment)
+        source_volume = self.compartment_volumes[source_idx]
+        dest_idx = self.compartment_names.index(to_compartment)
+        dest_volume = self.compartment_volumes[dest_idx]
+        if rate_law == 'first':
+            self.rhs_matrix[source_idx, source_idx] += - rate_constant / source_volume
+            self.rhs_matrix[source_idx, dest_idx] += rate_constant / dest_volume
+            self.rhs_matrix[dest_idx, source_idx] += rate_constant / source_volume
+            self.rhs_matrix[dest_idx, dest_idx] += - rate_constant / dest_volume
+        if rate_law == 'zero':
+            self.rhs_cst_vector[source_idx] += -rate_constant
+            self.rhs_cst_vector[dest_idx] += +rate_constant
+        else:
+            raise NotImplementedError("We only support first or zero order fluxes!")
         
+    def add_clearance(self, from_compartment, rate_constant, rate_law='first'):
+        source_idx = self.compartment_names.index(from_compartment)
+        source_volume = self.compartment_volumes[source_idx]
+        if rate_law == 'first':
+            self.rhs_matrix[source_idx, source_idx] += - rate_constant / source_volume
+        if rate_law == 'zero':
+            self.rhs_cst_vector[source_idx] += -rate_constant
+        else:
+            raise NotImplementedError("We only support first or zero order clearances!")
+              
+
 
