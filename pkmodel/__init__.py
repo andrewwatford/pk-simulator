c9cb0ddb63cef4b3e781c5bf7b2901d754bbb291
Stas <stanislavs.kurass@dtc.ox.ac.uk>
2025-10-28 09:56:51 +0000

    added Compartment obejct to init.py

--- pkmodel/__init__.py
+++ pkmodel/__init__.py
@@ -7,7 +7,4 @@ of Parmokinetic (PK) models
 # Import version info
 from .version_info import VERSION_INT, VERSION  # noqa
 
-# Import main classes
-from .model import Model    # noqa
-from .protocol import Protocol    # noqa
-from .solution import Solution     # noqa
+from Compartment import Compartment
