4419c44fa238c809ddae33b72d779ec8fa2a8c0d
andrewwatford <andrew.watford@dtc.ox.ac.uk>
2025-10-28 09:43:56 +0000

    deleted out-of-date functions

--- pkmodel/Flux.py
+++
@@ -1,5 +0,0 @@
-class Flux:
-    def __init__(self, source_compartment_name, dest_compartment_name, rate_function):
-        self.source_compartment_name = source_compartment_name
-        self.dest_compartment_name = dest_compartment_name
-        self.rate_function = rate_function
