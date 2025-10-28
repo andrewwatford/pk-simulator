16a0e38fa80583a2bea5ff997e2024cca014e8bb
Ambre Brabant <ambre.brabant@univ.ox.ac.uk>
2025-10-28 14:38:20 +0000

    Unit tests (matrix construction tests 1)

--- pkmodel/tests/test_compartment_model.py
+++ pkmodel/tests/test_compartment_model.py
@@ -36,3 +36,75 @@ class TestCompartmentModel:
                 compartment_names   = ['central','peripheral'],
                 compartment_volumes = [1,2,3])
     
+    @pytest.mark.parametrize(
+        "compartment_names, compartment_volumes, flux_dict_list, clearance_dict_list, expected_matrix, expected_cst_vector",
+        [
+            # Test case 1: two compartments, one first-order flux, no clearances
+            (   
+                ['central', 'peripheral'],
+                [22, 7],
+                [{
+                    'from_compartment': 'central',
+                    'to_compartment': 'peripheral',
+                    'rate_constant': 1,
+                    'rate_law': 'first'}],
+                [{
+                    'from_compartment': 'central',
+                    'rate_constant': 0,
+                    'rate_law': 'zero'
+                }],
+                [
+                [-1/22, 1/7],
+                [1/22, -1/7]
+                ],
+                [0, 0]
+            ),
+            # Test case 2: two compartments, one first-order flux, one first-order clearance,
+            (   
+                ['central', 'peripheral'],
+                [22, 7],
+                [{
+                    'from_compartment': 'central',
+                    'to_compartment': 'peripheral',
+                    'rate_constant': 1,
+                    'rate_law': 'first'},
+                    ],
+                [{
+                    'from_compartment': 'central',
+                    'rate_constant': 2,
+                    'rate_law': 'first'
+                }],
+                [
+                [-3/22, 1/7],
+                [1/22, -1/7]
+                ],
+                [0, 0]
+            ),
+            # Test case 3: subcutaneous dosing. three compartments, 
+            # Try to make edge cases for 2d and 3d
+            # Make tests for 3d
+        ]
+    )
+    def test_matrix_and_constant_vector(self, compartment_names, compartment_volumes, flux_dict_list, clearance_dict_list, expected_matrix, expected_cst_vector):
+        """
+        Tests that the RHS matrix and constant vector are correctly constructed
+        after various combinations of zero- or first-order fluxes and clearances are added
+        """      
+
+        # Initialise compartment model
+        model = pk.CompartmentModel(
+            compartment_names   = compartment_names,
+            compartment_volumes = compartment_volumes)
+
+        # Add fluxes
+        for flux_dict in flux_dict_list:
+            model.add_flux(**flux_dict)
+        
+        # Add clearances
+        for clearance_dict in clearance_dict_list:
+            model.add_clearance(**clearance_dict)
+
+        npt.assert_array_almost_equal(model.rhs_matrix, expected_matrix)
+        npt.assert_array_almost_equal(model.rhs_cst_vector, expected_cst_vector)
+        
+    
