e0ba76833b295d6897b821fe4087dace48603988
Ambre Brabant <ambre.brabant@univ.ox.ac.uk>
2025-10-28 13:49:23 +0000

    Unit tests - a) changed test_create(), b) wrote test_create_with_invalid_inputs

--- pkmodel/tests/test_compartment_model.py
+++ pkmodel/tests/test_compartment_model.py
@@ -1,7 +1,8 @@
-import unittest
+import numpy.testing as npt
+import pytest
 import pkmodel as pk
 
-class ComparmentTest(unittest.TestCase):
+class TestCompartmentModel:
     """
     Tests the CompartmentModel class.
     """
@@ -19,11 +20,19 @@ class ComparmentTest(unittest.TestCase):
             compartment_volumes = list(compartments_dict.values()))
         
         # Check if attributes are stored correctly in a model object
-        self.assertEqual(
-            model.compartment_names,
+        npt.assert_array_equal(model.compartment_names,
             ["central", "peripheral"])
-        self.assertEqual(
-            model.compartment_volumes,
+        npt.assert_array_equal(model.compartment_volumes,
             [22, 7])
         
+    def test_create_with_invalid_inputs(self):
+        """
+        Tests CompartmentModel creation with mismatched lengths of names and volumes.
+        """
+
+        # Test with mismatched lengths of names and volumes
+        with pytest.raises(ValueError):
+            pk.CompartmentModel(
+                compartment_names   = ['central','peripheral'],
+                compartment_volumes = [1,2,3])
     
