3b2ea454d0be89d35788eb1584cf19bc4d030ee0
Stas <stanislavs.kurass@dtc.ox.ac.uk>
2025-10-30 17:11:51 +0000

    fixed module imports in some tests

--- pkmodel/tests/test_compartment_model.py
+++ pkmodel/tests/test_compartment_model.py
@@ -4,6 +4,7 @@ import logging
 import pkmodel as pk
 import numpy as np
 
+from pkmodel.CompartmentModel import CompartmentModel, Compartment, Flux, Clearance, Dosage
 
 @pytest.fixture()
 def config_1():
@@ -79,7 +80,7 @@ def flux_1(comp_1, comp_2):
     Do not set scope to module, session, or class - because fixture will get modified
     by several different unit tests
     """
-    return pk.Flux(id="c_p",
+    return Flux(id="c_p",
                    source=comp_1,
                    dest=comp_2,
                    rate_constant=5.0,
@@ -94,7 +95,7 @@ def clearance_1(comp_1):
     Do not set scope to module, session, or class - because fixture will get modified
     by several different unit tests
     """
-    return pk.Clearance(id='central_clearance',
+    return Clearance(id='central_clearance',
                         source=comp_1,
                         rate_constant=5.0,
                         rate_law="first")
@@ -107,7 +108,7 @@ def dosage_1(comp_1):
     Do not set scope to module, session, or class - because fixture will get modified
     by several different unit tests
     """
-    return pk.Dosage(id='central_dosage',
+    return Dosage(id='central_dosage',
                      dest=comp_1,
                      regime="constant",
                      rate_constant=1.0)
@@ -268,7 +269,7 @@ class TestCompartmentModel:
         """
         # Try to add a flux with an invalid rate law
         with pytest.raises(ValueError):
-            cmodel_1.add_flux(pk.Flux(id='test',
+            cmodel_1.add_flux(Flux(id='test',
                                       source=cmodel_1.compartments['central'],
                                       dest=cmodel_1.compartments['peripheral'],
                                       rate_constant=1,
