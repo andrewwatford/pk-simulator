a86d2c9d9dabd5d112089702f011317e3631cd71
andrewwatford <andrew.watford@dtc.ox.ac.uk>
2025-10-30 11:16:21 +0000

    Updated basic_example.py

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,21 +1,50 @@
 import matplotlib.pyplot as plt
-from pkmodel.builtin_fluxes import constant_dose
 from pkmodel.CompartmentModel import CompartmentModel
 
-# Define model
-volumes_dict = {'central': 22, 'peripheral': 7}
-model = CompartmentModel(compartment_names = list(volumes_dict.keys()), compartment_volumes = list(volumes_dict.values()))
-# Adding fluxes, dosage, and clearances
-model.add_flux(from_compartment = 'central', 
-               to_compartment = 'peripheral', 
-               rate_law = 'first', # or 'zero'
-                rate_constant = 5)
-model.add_dosage(compartment_name = 'central', dosage_func = constant_dose(1))
-model.add_clearance(from_compartment = 'central', 
-                    rate_law = 'first', # or 'zero'
-                    rate_constant = 5)
-# running the model
-out = model.run(y0 = [0, 0], t_span = [0, 30])
+# Define model config
+config = {
+
+    "compartments": {
+        "central": 22.0,
+        "peripheral": 7.0,
+    },
+
+    "fluxes": {
+        "c_p": {
+            "source":"central",
+            "dest": "peripheral",
+            "rate_constant": 5.0,
+            "nature":"bidirectional",
+            "rate_law":"first"
+        }
+    },
+
+    "clearances": {
+        "central_clearance":{
+            "source":"central",
+            "rate_constant": 5.0,
+            "rate_law":"first"
+        }
+    },
+
+    "dosages": {
+        "central_dosage":{
+            "dest":"central",
+            "regime":"constant",
+            "rate_constant": 1.0,
+        }
+    }
+}
+
+# Instantiate the model object
+model = CompartmentModel.from_config(config)
+# Build and solve the model
+model.build_linear_rhs()
+# Initial conditions and volumes
+y0 = [0, 0]  # Initial mass in each compartment
+t_span = [0, 30]  # Time span for the simulation
+# Run the simulation
+result = model.run(t_span, y0)
 # Plots
-fig, axs = model.plot_all(out)
+fig, axs = model.plot_all(result)
 plt.savefig('./example.png')
