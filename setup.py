71fbe0cb6f3d9dd92c6e89d39d9284d890d482ca
andrewwatford <andrew.watford@dtc.ox.ac.uk>
2025-10-31 15:57:08 +0000

    added additional sphinx dependencies

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
             # Sphinx for doc generation. Version 1.7.3 has a bug:
             'sphinx>=1.5, !=1.7.3',
             # Nice theme for docs
-            'sphinx_rtd_theme',
+            'sphinx_rtd_theme', 'sphinx_autodoc_typehints', 'sphinx_book_theme'
         ],
         'dev': [
             # Flake8 for code style checking
